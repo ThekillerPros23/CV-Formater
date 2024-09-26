cf66356da4c15c4f45bbd33b4db264654ee57229
Roberto Goddard <mgroberto38@hotmail.com>
2024-09-26 17:09:41 -0500

    advance - 1.0.1
    DATA STRUCTURE
    DATA COMPACTION
    BETTER LETTER

--- firebase_data.py
+++ firebase_data.py
@@ -3,14 +3,11 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 
 # Use a service account.
-cred = credentials.Certificate('dev-portal-logistic-firebase-adminsdk-mtvp2-bbadfb4ad5.json')
-
-app = firebase_admin.initialize_app(cred)
-
-db = firestore.client()
-
-users_ref = db.collection("marinos")
-docs = users_ref.stream()
-
-for doc in docs:
-    print(f"{doc.id} => {doc.to_dict()}")
+class FirebaseData():
+    def database(self, query):
+        self.cred = credentials.Certificate('dev-portal-logistic-firebase-adminsdk-mtvp2-bbadfb4ad5.json')
+        app = firebase_admin.initialize_app(self.cred)
+        self.db = firestore.client()
+        users_ref = self.db.collection(query)
+        docs = users_ref.stream()
+        return self.docs
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fpdf import FPDF
 from flask import Flask, jsonify, request
 from datetime import datetime
+from firebase_data import *
 
+database = FirebaseData()
 class PDF(FPDF):
     def header(self):
         # Solo agregar el encabezado en la primera página
@@ -9,26 +11,24 @@ class PDF(FPDF):
             self.image("LOGISTIC-SinFondo.png", 160, 8, 33)  # Alineado a la derecha
     def footer(self):
         self.set_y(-20)
+        self.set_font('calibri', 'I', 8)
 
-        # Configuración de fuente para el footer
-        self.set_font('calibri', 'I', 10)
-
-        # Texto alineado a la derecha
-        # Mover el cursor al borde derecho
-        self.set_x(-60)  # Ajusta el valor para alinear mejor al borde derecho
-
-        # Agregar las líneas de información del pie de página
-        self.cell(0, 5, 'Código: F-PMSSA-01-E', ln=True, align='R')
-        self.set_x(-60)  # Repetimos para alinear cada línea
-        self.cell(0, 5, 'Revisión: 00', ln=True, align='R')
+        # Código
         self.set_x(-60)
-        self.cell(0, 5, 'Fecha: 17 de mayo de 2022', ln=True, align='R')
-        self.set_x(-80)
+        self.cell(0, 3.5, 'Código: F-PMSSA-01-E', ln=True, align='R')
 
-        # Número de página actual vs total de páginas
+        # Revisión
+        self.set_x(-60)
+        self.cell(0, 3.5, 'Revisión: 00', ln=True, align='R')
+
+        # Fecha
+        self.set_x(-60)
+        self.cell(0, 3, 'Fecha: 17 de mayo de 2022', ln=True, align='R')
+
+        # Número de página
+        self.set_x(-30)
         page_text = f'Página {self.page_no()} de {{nb}}'
-        self.cell(0, 5, page_text, ln=True, align='R')
-   
+        self.cell(0, 3, page_text, ln=True, align='R')
 app = Flask(__name__)
 
 @app.route('/pdf_render', methods=['GET'])
@@ -66,23 +66,26 @@ def pdf_render():
     pdf = PDF(orientation='P', unit='mm', format='A4')
     pdf.add_font('calibri', '', 'calibri.ttf', uni=True)
     pdf.add_font('calibri', 'I','calibrii.ttf',uni=True)
+    pdf.add_font('calibri', 'BU','calibri.ttf',uni=True)
+    pdf.add_font('calibri', 'B','calibrib.ttf',uni=True)
+    
     pdf.add_page()
     pdf.alias_nb_pages()
     # Agregar contenido al PDF
     pdf.set_xy(0, 20)  # Ajustar la posición para el título
-    pdf.set_font('helvetica', '', 22)
+    pdf.set_font('calibri', '', 22)
     pdf.cell(0, 10, 'SEAFARER APPLICATION FORM', align='C')
 
     pdf.set_xy(55, 30)  # Ajustar la posición para el siguiente texto
-    pdf.set_font('Arial', '', 11)
+    pdf.set_font('calibri', '', 11)
     pdf.cell(30, 10, 'POSITION APPLYING FOR RANK: ')
     
-    pdf.set_xy(124, 30)
-    pdf.set_font('Arial', 'BU', 11)
+    pdf.set_xy(116, 30)
+    pdf.set_font('calibri', 'BU', 11)
     pdf.cell(60, 10, 'MESSMAN')
 
     pdf.set_xy(55, 40)
-    pdf.set_font('Arial', '', 11)
+    pdf.set_font('calibri', '', 11)
     pdf.cell(55, 10, '1. PERSONAL INFORMATION')
 
 
@@ -93,54 +96,55 @@ def pdf_render():
     cell_width = 50
     big_cell_width = 100
     height = 7
-    pdf.set_font('calibri', '', 10) 
+    pdf.set_font('calibri', '', 8) 
     # Encabezado para Nombres
-    pdf.cell(w=cell_width, h=height, txt='NAME', border=1, align='L')
-    pdf.cell(w=cell_width, h=height, txt=str(name), border=1, align='C')
-    pdf.cell(w=cell_width, h=height, txt=str(second_name), border=1, align='C', ln=1)
-    pdf.set_font('calibri', '', 10) 
+    
+    pdf.cell(w=40, h=height, txt='NAME', border=1, align='L')
+    pdf.cell(w=40, h=height, txt=str(name), border=1, align='C')
+    pdf.cell(w=40, h=height, txt=str(second_name), border=1, align='C', ln=1)
+    pdf.set_font('calibri', '', 8) 
     # Encabezado para Apellidos
     pdf.set_xy(50, 57)  
-    pdf.cell(w=cell_width, h=height, txt='SURNAMES', border=1, align='L')
-    pdf.cell(w=cell_width, h=height, txt=str(lastname), border=1, align='C')
-    pdf.cell(w=cell_width, h=height, txt=str(second_lastname), border=1, align='C', ln=1)
-    pdf.set_font('calibri', '', 10) 
+    pdf.cell(w=40, h=height, txt='SURNAMES', border=1, align='L')
+    pdf.cell(w=40, h=height, txt=str(lastname), border=1, align='C')
+    pdf.cell(w=40, h=height, txt=str(second_lastname), border=1, align='C', ln=1)
+    pdf.set_font('calibri', '', 8) 
     # Fecha de nacimiento
     pdf.set_xy(50, 64)  
-    pdf.multi_cell(w=cell_width, h=6.5, txt='DATE OF BIRTH\n(YYYY-MM-DD)', border=1, align='C')
+    pdf.multi_cell(w=40, h=6.5, txt='DATE OF BIRTH\n(YYYY-MM-DD)', border=1, align='C')
 
     # Llenar la fecha de nacimiento
-    pdf.set_xy(100, 64) 
-    pdf.cell(w=big_cell_width, h=13, txt=str(formatted_timestamp), border=1, align='C', ln=1)
+    pdf.set_xy(90, 64) 
+    pdf.cell(w=80, h=13, txt=str(formatted_timestamp), border=1, align='C', ln=1)
 
     # Nacionalidad
     pdf.set_xy(50, 77)  
-    pdf.cell(w=cell_width, h=height, txt='NATIONALITY', border=1, align='L')
-    pdf.cell(w=big_cell_width, h=height, txt=str(nationality), border=1, align='C', ln=1)
+    pdf.cell(w=40, h=height, txt='NATIONALITY', border=1, align='L')
+    pdf.cell(w=80, h=height, txt=str(nationality), border=1, align='C', ln=1)
 
     # Sexo y Estado Civil
     pdf.set_xy(50, 84)  
-    pdf.cell(w=cell_width, h=7, txt='SEX', border=1, align='L')
+    pdf.cell(w=40, h=7, txt='SEX', border=1, align='L')
     pdf.cell(w=20, h=7, txt=str(sex), border=1, align='C')
-    pdf.cell(w=45, h=7, txt='CIVIL STATUS', border=1, align='L')
-    pdf.cell(w=35, h=7, txt=str(civil_status), border=1, align='C', ln=1)
+    pdf.cell(w=30, h=7, txt='CIVIL STATUS', border=1, align='L')
+    pdf.cell(w=30, h=7, txt=str(civil_status), border=1, align='C', ln=1)
     
     # Espaciado y otras celdas
     pdf.set_xy(50, 91)
-    pdf.cell(w=30, h=7, txt='HEIGHT (Ft/in)', border=1, align='L')
+    pdf.cell(w=25, h=7, txt='HEIGHT (Ft/in)', border=1, align='L')
     pdf.cell(w=20, h=7, txt=str(sex), border=1, align='C')
-    pdf.cell(w=30, h=7, txt='WEIGHT (Lb)', border=1, align='L')
-    pdf.cell(w=20, h=7, txt=str(civil_status), border=1, align='C')
-    pdf.cell(w=20, h=7, txt='BMI', border=1, align='L')
-    pdf.cell(w=30, h=7, txt=str(civil_status), border=1, align='C', ln=1)
+    pdf.cell(w=22, h=7, txt='WEIGHT (Lb)', border=1, align='L')
+    pdf.cell(w=18, h=7, txt=str(civil_status), border=1, align='C')
+    pdf.cell(w=15, h=7, txt='BMI', border=1, align='L')
+    pdf.cell(w=20, h=7, txt=str(civil_status), border=1, align='C', ln=1)
 
     pdf.ln(5)
-    pdf.set_font('calibri', '', 9.5)
+    pdf.set_font('calibri', '', 8)
     pdf.multi_cell(w=40, h=7, txt="COMPLETE HOME ADDRESS", border=1, align="L")
     pdf.set_xy(50, 103)
-    pdf.cell(w=50, h=13, txt="", border=1, align="C")
-    pdf.cell(w=50, h=13, txt="NEARLY AIRPORT", border=1, align="L")
-    pdf.cell(w=50, h=14, txt="", border=1, align="C", ln=1)
+    pdf.cell(w=50, h=7, txt="", border=1, align="C")
+    pdf.cell(w=50, h=7, txt="NEARLY AIRPORT", border=1, align="L")
+    pdf.cell(w=50, h=7, txt="", border=1, align="C", ln=1)
     pdf.cell(w=30, h=7, txt="PHONE/CELL", border=1, align="C")
     pdf.cell(w=30, h=7, txt="", border=1, align="L")
     pdf.cell(w=30, h=7, txt="WHATSAPP", border=1, align="C")
@@ -155,7 +159,7 @@ def pdf_render():
     pdf.cell(w=20, h=7, txt="OTHERS", border=1, align="L")
     pdf.cell(w=30, h=7, txt=""+"%", border=1, align="R", ln=1)
     pdf.ln(5)
-
+    pdf.set_font('calibri','',8)
     pdf.cell(w=0, h=7, txt="MARLINS / LANGUAGE -TEST", border=1, align="C",ln=1)
     pdf.cell(w=60, h=7, txt="TOTAL %", border=1, align="C")
     pdf.cell(w=60, h=7, txt="ISSUE DATE", border=1, align="C")
@@ -165,15 +169,15 @@ def pdf_render():
     pdf.cell(w=70, h=7, txt="", border=1, align="C",ln=1)
     
     pdf.ln(5)
-    pdf.set_xy(40,160)
+    pdf.set_font('calibri','',8)
     pdf.cell(0,10,txt="2. EMERGENCY CONTACT / NEXT OF KIN0", border=0, align='L')
     pdf.ln(15)
-    pdf.cell(w=0,h=14,txt="EMERGENCY CONTACT / NEXT OF KIN", border=1, align='C',ln=1)
-    pdf.cell(w=40,h=14,txt="RELATIONSHIP", border=1, align='C')
-    pdf.cell(w=50,h=14,txt="COMPLETE NAME", border=1, align='C')
+    pdf.cell(w=0,h=7,txt="EMERGENCY CONTACT / NEXT OF KIN", border=1, align='C',ln=1)
+    pdf.cell(w=40,h=7,txt="RELATIONSHIP", border=1, align='C')
+    pdf.cell(w=50,h=7,txt="COMPLETE NAME", border=1, align='C')
     pdf.multi_cell(w=60,h=7,txt="TELEPHONE NUMBER / MOBILE", border=1, align='C')
-    pdf.set_xy(160,189)
-    pdf.cell(w=40,h=14,txt="ADDRESS", border=1, align='C',ln=1)
+    pdf.set_xy(160,177)
+    pdf.cell(w=40,h=7,txt="ADDRESS", border=1, align='C',ln=1)
     for fila in datos:
     # Recorrer cada columna y valor de la fila
         for i, (columna, valor) in enumerate(fila.items()):
@@ -187,7 +191,7 @@ def pdf_render():
 
     anchuras_columnas = [28, 28, 24, 21, 18, 18, 23, 30]  
     altura_fila = [7,7,7,7,14,14,7,7]
-    pdf.set_font('arial','', 9)
+    
     titulos_columnas = [
     'DATE ON\n(MM/DD/YYYY)',
     'DATE OFF\n(MM/DD/YYYY)',
@@ -200,18 +204,38 @@ def pdf_render():
 ]
     align_type = ['C', 'C', 'C', 'L', 'C', 'L', 'C', 'C']
     pdf.set_xy(x_inicial, y_inicial)
+    pdf.set_font('calibri','', 8)
     for i in range(len(titulos_columnas)):
         pdf.multi_cell(w=anchuras_columnas[i], h=altura_fila[i], txt=titulos_columnas[i], align=align_type[i], border=1)
         x_inicial += anchuras_columnas[i]
         pdf.set_xy(x_inicial, y_inicial)
-    pdf.ln(20)
+    pdf.ln(14)
+
+    datosNuevos = [
+    ['01/01/2023', '01/02/2023', 'Company A', 'Vessel A', '1234567', '5000 / 3000', 'Type A', 'Captain'],
+    ['02/01/2023', '02/02/2023', 'Company B', 'Vessel B', '2345678', '6000 / 3500', 'Type B', 'First Officer'],
+     ['02/01/2023', '02/02/2023', 'Company B', 'Vessel B', '2345678', '6000 / 3500', 'Type B', 'First Officer'],
+    
+    # Agrega más filas según sea necesario
+]
+
+# Dibujar los datos
+    nuevaaltura_fila = [7,7,7,7,7,7,7,7]
+    for fila in datosNuevos:
+        x_inicial = pdf.get_x()
+        y_inicial = pdf.get_y()
+        for i in range(len(fila)):
+            pdf.multi_cell(w=anchuras_columnas[i], h=nuevaaltura_fila[i], txt=fila[i], align=align_type[i], border=1)
+            x_inicial += anchuras_columnas[i]
+            pdf.set_xy(x_inicial, y_inicial)
+        pdf.ln(nuevaaltura_fila[0]) 
     
     pdf.cell(0, 10, txt='4. Personal Documentation / Seafarer Documentation', align='L')
     pdf.ln(10)  
 
 
     pdf.cell(w=0, h=7, txt='PERSONAL DOCUMENTATION / SEAFARER DOCUMENTATION', align='C', border=1, ln=1)
-    pdf.set_font('Calibri', '', 10)
+    pdf.set_font('Calibri', '', 8)
 
 
     pdf.multi_cell(w=30, h=7, txt='TYPE OF DOCUMENT / ID', align='C', border=1)
@@ -231,6 +255,7 @@ def pdf_render():
     pdf.multi_cell(w=30,h=7,txt='DATE OF ISSUE (MM/DD/YYYY)', align='C', border=1)
     pdf.multi_cell(w=30,h=7,txt='DATE OF EXPIRY(MM/DD/YYYY)', align='C', border=1)
     pdf.ln(5)
+    pdf.set_font('calibri', '', 8)
     pdf.cell(0,10, txt='6. WORK EXPERIENCE ONSHORE', align='L')
     pdf.ln(10)
     pdf.multi_cell(w=30,h=7,txt='DATE ON(MM/DD/YYYY)', align='C', border=1)
@@ -240,7 +265,33 @@ def pdf_render():
     pdf.multi_cell(w=30,h=7,txt='RANK/POSITION', align='C', border=1)
     pdf.multi_cell(w=30,h=7,txt='REASON FOR LEAVING', align='C', border=1)
     pdf.multi_cell(w=30,h=7,txt='NAME OF CONTACT PERSON & TELEPHONE NUMBER', align='C', border=1)
-    
+    pdf.ln(10)
+    pdf.cell(0,10, txt='7. HIGHEST LEVEL OF EDUCATION / OTHER TRAINING OR CERTIFICATE', align='L')
+    pdf.ln(10)
+    pdf.cell(w=0, h=7,txt='HIGHEST LEVEL OF EDUCATION / OTHER TRAINING OR CERTIFICATE', align='C', border=1)
+    pdf.ln(10)
+    pdf.multi_cell(w=30,h=7,txt='NAME OF EDUCATION INSTITUTION / TECHNICAL INSTITUTE / UNIVERSITY', align='C', border=1)
+    pdf.multi_cell(w=30,h=7,txt='OBTAINED TITLE OR GRADE', align='C', border=1)
+    pdf.multi_cell(w=30,h=7,txt='DATE ON(MM/DD/YYYY)', align='C', border=1)
+    pdf.multi_cell(w=30,h=7,txt='DATE OFF(MM/DD/YYYY)', align='C', border=1)
+    pdf.ln(10)
+    pdf.cell(0,10, txt='8. VACCINATION BOOK', align='L')
+    pdf.ln(10)
+    pdf.cell(w=0, h=6,txt='VACCINATION BOOK', align='C', border=1)
+    pdf.ln(10)
+    pdf.set_font('calibri','',8)
+    pdf.cell(w=40,h=6,txt="TYPE OF VACCINE", border=1, align='C')
+    pdf.cell(w=40,h=6,txt="COUNTRY", border=1, align='C')
+    pdf.cell(w=40,h=6,txt="DOZE", border=1, align='C')
+    pdf.multi_cell(w=40,h=3,txt='DATE OF ISSUE\n(MM / DD / YYYY)', align='C', border=1)
+    pdf.multi_cell(w=30,h=6,txt='VACCINATION MARK', align='C', border=1)
+    pdf.ln(10)
+    pdf.cell(0,10, txt='9. SKILLS / RESPONSIBILITIES / LEARNING EXPERIENCE / ACHIEVEMENTS', align='L')
+    pdf.ln(10)
+    pdf.cell(w=110,h=6,txt="SKILLS / RESPONSIBILITIES / LEARNING EXPERIENCE / ACHIEVEMENTS", border=1, align='L')
+    pdf.cell(w=40,h=6,txt="YES", border=1, align='C')
+    pdf.cell(w=40,h=6,txt="NO", border=1, align='C')
+   
     pdf.output('hoja.pdf')
 
     return jsonify({"message": "PDF generated successfully!"})
