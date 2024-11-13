81eeb7933b62bda5bcff0f9bfdd35816d41b6bb8
Roberto Goddard <mgroberto38@hotmail.com>
2024-11-13 13:35:35 -0500

    d

--- formatsSeafarers/bosun.py
+++ formatsSeafarers/bosun.py
@@ -329,7 +329,7 @@ class BosunSeafarers():
         cell_height = 7  # Altura base para cada línea de texto
 
         # Anchos específicos para cada columna
-        anchuras = [40, 50, 60, 40]
+        anchuras = [30, 60, 50, 50]
 
         # Dibujar encabezado
         pdf.cell(w=anchuras[0], h=cell_height, txt="RELATIONSHIP", border=1, align='C', fill=True)
