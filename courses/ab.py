a325ea188aa31dd8c40c0fbce5dfcfd97823741f
Roberto Goddard <mgroberto38@hotmail.com>
2024-11-13 14:14:40 -0500

    change

--- courses/ab.py
+++ courses/ab.py
@@ -2,20 +2,20 @@ class AbCourses():
     def courses(self):
         courses = [
     "Basic Safety Maritime Training Course (BST)",
-    "Proficiency in personal Survival Techniques 1.19",
-    "Fire prevention and firefighting 1.20",
-    "Elementary first Aid 1.13",
-    "Personal Safety and social responsibilities 1.21",
-    "Security Awareness Training for all seafarers Course 3.27",
-    "Security Awareness Training for all seafarers with designated security Duties Course 3.26",
-    "Safety training for personnel proving direct services to passenger in passenger spaces 1.44",
-    "Passenger ship Crowd Management Training 1.41",
-    "Passenger ship crisis management training 1.42",
-    "Passenger Safety, cargo safety and Hull Integrity Training. 1.29",
-    "Proficiency in the Management of Survival Crafts and rescue boats Course 1.23",
-    "Basic Training for Oil and chemical cargo tanker Operations 1.01",
+    "Proficiency in Personal Survival Techniques 1.19",
+    "Fire Prevention and Firefighting 1.20",
+    "Elementary First Aid 1.13",
+    "Personal Safety and Social Responsibilities 1.21",
+    "Security Awareness Training for all Seafarers Course 3.27",
+    "Security Awareness Training for all Seafarers with Designated Security Duties Course 3.26",
+    "Safety Training for Personnel Providing Direct Services to Passenger in Passenger Spaces 1.44",
+    "Passenger Ship Crowd Management Training 1.41",
+    "Passenger Ship Crisis Management Training 1.42",
+    "Passenger Safety, Cargo Safety and Hull Integrity Training 1.29",
+    "Proficiency in the Management of Survival Crafts and Rescue Boats Course 1.23",
+    "Basic Training for Oil and Chemical Cargo Tanker Operations 1.01",
     "Advanced Fire Fighting 2.03",
-    "Ratings forming part of a navigational watch"
+    "Ratings Forming Part of a Navigational Watch"
 ]
 
         return courses
--- courses/officer.py
+++ courses/officer.py
@@ -1,31 +1,31 @@
 class OfficerCourses():
         def courses(self):
             courses = [
-    "Basic Safety Maritime Training Course (BST)",
-    "Proficiency in personal Survival Techniques 1.19",
-    "Fire prevention and firefighting 1.20",
-    "Elementary first Aid 1.13",
-    "Personal Safety and social responsibilities 1.21",
-    "Ship security Officer 3.19",
-    "Proficiency in the Management of Survival Crafts and rescue boats Course 1.23",
+   "Basic Safety Maritime Training Course (BST)",
+    "Proficiency in Personal Survival Techniques 1.19",
+    "Fire Prevention and Firefighting 1.20",
+    "Elementary First Aid 1.13",
+    "Personal Safety and Social Responsibilities 1.21",
+    "Ship Security Officer 3.19",
+    "Proficiency in the Management of Survival Crafts and Rescue Boats Course 1.23",
     "Proficiency in Fast Rescue Boats 1.24",
-    "Advanced firefighting 2.03",
+    "Advanced Firefighting 2.03",
     "Medical Care 1.15",
     "Engine Resource Management",
     "Engine Room Simulator",
-    "Ship simulator and bridge teamwork 1.22",
+    "Ship Simulator and Bridge Teamwork 1.22",
     "High Voltage Installations",
     "Marine Environmental Awareness 1.38",
-    "Leadership and teamwork 1.39",
+    "Leadership and Teamwork 1.39",
     "Bridge Resource Management",
     "ECDIS 1.27",
     "GMDSS 1.25",
     "ARPA 1.07",
     "RADAR 1.08",
-    "Safety training for personnel proving direct services to passenger in passenger spaces 1.44",
-    "Passenger ship Crowd Management Training 1.41",
-    "Passenger ship crisis management training 1.42",
-    "Passenger Safety, cargo safety and Hull Integrity Training. 1.29"
+    "Safety Training for Personnel Providing Direct Services to Passenger in Passenger Spaces 1.44",
+    "Passenger Ship Crowd Management Training 1.41",
+    "Passenger Ship Crisis Management Training 1.42",
+    "Passenger Safety, Cargo Safety and Hull Integrity Training 1.29"
 ]
 
             return courses
